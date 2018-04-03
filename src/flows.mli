f388aa31728eba7cd196ad3524784fa84733df96
Francois-Rene Rideau <fare@tunes.org>
2018-04-03 11:25:24 -0400

    Dump partial second flow into flows

--- src/flows.mli
+++ src/flows.mli
@@ -129,3 +129,97 @@ val commit_side_chain_state : unit -> unit legi_result
 (* flow operations *)
 
 type x_facilitator_preconditions
+
+
+type digest_t = Int256_t
+
+type main_chain_height_t = Int32.t (* Number of consensus, if linear block-based. Timestamp if DAG-based? *)
+type main_chain_header_t = digest_t (* Hash of the header for the height. *)
+type duration_t = Int32.t (* difference for main_chain_height_t *)
+
+(* Transaction header.
+   Every transaction in a Legicash side-chain starts with the same header.
+   Future Optimization: when storing the transaction in memory or on disk,
+   common data that can be deduced from context is omitted from storage and computed instead.
+   But when signing the transaction and showing the evidence of the transaction to clients,
+   the full data is included.
+ *)
+type tx_header_t = {
+    message_type : Int32.t; (* Type of the message *)
+    tx_revision : revision_t; (* *)
+  }
+
+(* Request header.
+   Every client request that initiates a transaction comes with a request window,
+   that puts a cap on the validity of the request in terms of inclusion in the main chain.
+   Thus, the other parties cannot hold the requestor's resource indefinitely on hold.
+   Additionally, the request may contains reference to the root of the main chain consensus,
+   so that it is clear which fork the transaction happens in;
+   in some cases, it might be OK to be active in multiple forks;
+   in other cases, it might lead to the requestor being punished in both.
+   Note that the request_window_t data could be summarized in a hash,
+   so the details can be omitted in future state logs, saving space;
+   but the content would still need to be published for present validation,
+   so that is a space loss in the short run (and/or for long-term archivers).
+   Alternatively, to save space, the root may not be stored in places where the validity
+   requires the root to be the same as *the* known consensual root at the given date.
+ *)
+type rx_header_t = {
+    root : main_chain_header_t;
+    date : main_chain_height_t;
+    timeout : duration_t;
+  }
+
+type account_status_t = {
+    rx_header : rx_header_t;
+    (* Number of times the account was previously closed or opened.
+       If the number is even, the account is closed.
+       If the number is odd, the account is open. *)
+    count : Int32;
+  }
+
+type account_status_confirmation_t = {
+    tx_header : side_chain_tx_header_t;
+    request: account_status_t signed;
+  }
+
+type message_for_enter_settlement_flow_t =
+  (* Step 1: Alice signs the new desired status and sends it to Trent. *)
+  | Account_status_change_request of account_status_t signed;
+  (* Step 2: Trent signs the request, stores the confirmation, sends the confirmation to Alice. *)
+  | Account_status_change_confirmation of account_status_confirmation_t signed;
+  (* Step 3: Alice sends money to Trent's contract on the main chain, using the normal mechanism,
+     Plus some annotation to signify the public_key of the address on the side-chain,
+     and including some fees to pre-pay UTXO consolidation.
+     Otherwise, her UTXO is locked and all she can do is take it out.
+   *)
+   (* Step 5: Trent commits transaction to side-chain *)
+Alice sends money to Trent's contract on the main chain, using the normal mechanism *)
+
+
+(* Actors: Alice (sender), Trent (facilitator)
+ *)
+
+
+
+Preconditions:
+* Alice has an account on the main chain with balance X+F+R.
+* Trent has an account for Alice (voluntary or involuntary) with balance Y (or no account, and balance 0)
+* Trent already has an open contract on the main
+
+Postconditions:
+* Alice has an account on the main chain with balance R.
+* Alice has a voluntary account on Trent's side-chain with balance X+Y.
+* Trent receives fee F from Alice.
+
+1. Alice sends X+F to Trent's contract on the main chain
+2. Trent write M1...
+3. Trent commits M1 to side-chain
+4. Trent sends M1 to Alice (OKish if fails)
+5. Trent commits M1 to main chain
+
+1 is enough for Alice to have her money back even if Trent fails.
+If 5 didn't happen, Alice still can get her money back, which
+the adversarial exit thing must take into account.
+
+ *)
