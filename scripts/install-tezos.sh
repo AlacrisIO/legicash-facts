467c74bb8500edd499c6af335d2a6296372d70d7
Paul Steckler <steck@stecksoft.com>
2018-04-09 09:50:29 -0400

    add ocp-json dev pin to tezos install

--- scripts/install-tezos.sh
+++ scripts/install-tezos.sh
@@ -13,6 +13,9 @@ opam install -y irmin jbuilder leveldb lwt mtime nocrypto && \
 opam install -y ocp-ocamlres ocplib-endian ocplib-json-typed && \
 opam install -y omake opam-installer ounit re ssl stringext
 
+# temporary; remove once dev ocp-json-typed becomes current
+opam pin -y add ocplib-json-typed --dev
+
 git clone https://gitlab.com/tezos/tezos.git && \
     cd tezos && \
     make && \
