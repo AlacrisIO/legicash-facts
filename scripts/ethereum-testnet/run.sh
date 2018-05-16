33690ee28584981085b2fbc670ab4c9c7737b09d
Paul Steckler <psteck@mit.edu>
2018-05-16 16:36:51 -0400

    add 'personal' API to running geth

--- scripts/ethereum-testnet/run.sh
+++ scripts/ethereum-testnet/run.sh
@@ -7,5 +7,5 @@ RPCPORT=8080
 DATADIR=geth-data
 
 geth --identity "LegicashEthereumTestNet" --datadir $DATADIR \
-      --nodiscover --maxpeers 0 --rpc --rpcapi "db,eth,net,web3,light" --rpcport $RPCPORT --rpccorsdomain "*" \
+      --nodiscover --maxpeers 0 --rpc --rpcapi "db,eth,net,web3,light,personal" --rpcport $RPCPORT --rpccorsdomain "*" \
       --port $PORT --networkid 17 --nat "any" --ipcpath .ethereum/geth.ipc > $DATADIR/testnet.log &
