a1b0731017d08cd026cd87afab66f51ca470da1e
Francois-Rene Rideau <fare@tunes.org>
2018-08-23 02:58:52 +0000

    Run nginx in user mode since we don't need port 80

--- src/endpoints/nginx/start.sh
+++ src/endpoints/nginx/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo nginx -p `pwd` -c conf/nginx.conf
+# NB: Running as a normal user. Otherwise, use sudo and uncomment the user directive in conf/nginx.conf
+nginx -p `pwd` -c conf/nginx.conf
