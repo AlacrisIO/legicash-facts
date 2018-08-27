03c782a8f99e821e4ebfe8500e38c9cc5041046f
Alex Coventry <alx@mit.edu>
2018-08-27 16:31:18 -0400

    Comments for Merkle_trie

--- src/legilogic_lib/merkle_trie.mli
+++ src/legilogic_lib/merkle_trie.mli
@@ -7,6 +7,7 @@ open Persisting
 open Types
 open Trie
 
+(** Signature for Trie which synthesizes digests from marshaled node values. *)
 module type TrieSynthMerkleS = sig
   include TrieSynthS with type t = digest
   val marshal_empty : unit marshaler
@@ -15,33 +16,49 @@ module type TrieSynthMerkleS = sig
   val marshal_skip : (int*int*key*digest) marshaler
 end
 
+(** Persistable merkle tree. [Key]s are bit paths (right/left), [Value]s are the
+   tree type. *)
 module TrieSynthMerkle (Key : UIntS) (Value : PersistableS)
   : TrieSynthMerkleS with type key = Key.t
                       and type value = Value.t
 
+(** Peristable merkle tree over persistable values of type [t] wrapped by type
+   [T] *)
 module type MerkleTrieTypeS = sig
   include TrieTypeS
   module Trie : PersistableS with type t = trie
   module T : PersistableS with type t = t
 end
 
+(** Persistable merkle tree with methods for recursive synthesization of node
+   digests. *)
 module MerkleTrieType (Key : UIntS) (Value : PersistableS)
     (Synth : TrieSynthMerkleS with type key = Key.t and type value = Value.t)
   : MerkleTrieTypeS with type key = Key.t
                      and type value = Value.t
 
+(** Signature for the merkle proof *)
 module type MerkleTrieProofS = sig
   type key
   type value
   type mtrie
-  type 'a pstep
+  type 'a pstep (* [pstep] ends up being identified with [MerkleTrie.step] *)
   type t =
-    { key : key
+    {
+      (** Database key for the leaf node *)
+      key : key
+    (** Root commitment (the actual merkle hash for the trie) *)
     ; trie : Digest.t
+    (** Hash value for the leaf *)
     ; leaf : Digest.t
+    (** Hashes to pair with in the proof, and their locations *)
     ; steps : (Digest.t pstep) list
     }
+  (** [get k mt] is the proof that the object referenced in the db by k is in
+     mt. *)
   val get : key -> mtrie -> t option
+  (** [check proof mt k v] verifies that [proof] is a proof for membership of
+     [v] in [mt], where [v] is referenced by [k] in the db. *)
   val check : t -> mtrie -> key -> value -> bool
   include YojsonableS with type t := t
 end
@@ -50,6 +67,7 @@ end
 module type MerkleTrieS = sig
   type key
   type value
+  (* Contains the logic for recursively computing merkle digest of tree *)
   module Synth : TrieSynthMerkleS with type key = key and type value = value
   module Type : TrieTypeS with type key = key and type value = value
   include TrieS
@@ -60,7 +78,9 @@ module type MerkleTrieS = sig
   include PersistableS
     with type t := t
 
+  (* Return the merkle commitment for the trie *)
   val trie_digest : t -> Digest.t
+  (* Return the merkle proof for the given path *)
   val path_digest : t path -> Digest.t path
 
   module Proof : MerkleTrieProofS
@@ -70,6 +90,7 @@ end
 module MerkleTrie (Key : UIntS) (Value : PersistableS)
   : MerkleTrieS with type key = Key.t and type value = Value.t
 
+(** Proofs that [elt]s are members of tries [mts].  *)
 module type MerkleTrieSetProofS = sig
   type elt
   type mts
@@ -83,6 +104,7 @@ module type MerkleTrieSetProofS = sig
   include YojsonableS with type t := t
 end
 
+(** Merkle tries where the only concern is membership, not tree location. *)
 module type MerkleTrieSetS = sig
   type elt
   module T : MerkleTrieS with type key = elt and type value = unit
@@ -91,7 +113,8 @@ module type MerkleTrieSetS = sig
   module Proof : MerkleTrieSetProofS
     with type elt = elt and type mts = t and type 'a pstep = 'a T.step
   val trie_digest : t -> Digest.t
-  val lens : elt -> (t, bool) Lens.t
+  (** get/set membership of [elt]s **)
+  val lens : elt -> (t, bool) Lens.t 
 end
 
 module MerkleTrieSet (Elt : UIntS) : (MerkleTrieSetS with type elt = Elt.t)
