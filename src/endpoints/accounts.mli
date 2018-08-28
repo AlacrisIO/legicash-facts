032ebf2d7bd95baef4fe19263c68c0cdc1549ed6
Paul Steckler <steck@stecksoft.com>
2018-08-28 15:21:33 -0400

    add MLI files

---
+++ src/endpoints/accounts.mli
@@ -0,0 +1,23 @@
+(* accounts.mli *)
+
+open Legilogic_lib
+open Signing
+
+open Legicash_lib
+open Side_chain
+open Side_chain_user
+
+val address_to_user_state_tbl : (Address.t,UserState.t)  Hashtbl.t
+(** table of current user states, maintained as transactions occur *)
+
+val trent_address : Address.t
+(** our faclitator's address *)
+
+val get_trent_state : unit -> FacilitatorState.t
+(** faciliator state, maintained as transactions occur *)
+
+val get_user_account : Address.t -> AccountState.t
+(** user's account state, maintained as transactions occur *)
+
+val get_user_name : Address.t -> string
+(** user's "friendly" name *)
---
+++ src/endpoints/actions.mli
@@ -0,0 +1,40 @@
+(* actions.mli *)
+
+open Legilogic_lib
+
+open Legicash_lib
+open Side_chain
+open Signing
+
+val error_json : string -> Yojsoning.yojson
+(** make JSON record with error *)
+
+val get_all_balances_on_trent : unit -> Yojsoning.yojson Lwt.t
+(** balances fora all users *)
+
+val get_transaction_rate_on_trent : unit -> Yojsoning.yojson
+(** timestamped transaction rate for last minute *)
+
+val get_proof : int -> Yojsoning.yojson
+(** Merkle proof for transaction with given tx_revision *)
+
+val apply_main_chain_thread : int -> Yojsoning.yojson
+(** view deposit/withdrawal thread result *)
+
+val deposit_to_trent : Address.t -> TokenAmount.t -> Yojsoning.yojson
+(** user, number of tokens *)
+
+val withdrawal_from_trent : Address.t -> TokenAmount.t -> Yojsoning.yojson
+(** user, number of tokens *)
+
+val payment_on_trent : Address.t -> Address.t -> TokenAmount.t -> Yojsoning.yojson Lwt.t
+(** payment between two users; first address is sender, second is recipient *)
+
+val get_balance_on_trent : Address.t -> Yojsoning.yojson
+(** balance for particular user *)
+
+val get_recent_transactions_on_trent : Address.t -> int option -> Yojsoning.yojson
+(** int is max number of transactions *)
+
+val get_status_on_trent_and_main_chain : Address.t -> Yojsoning.yojson Lwt.t
+(** user accounts information on side and main chains *)
