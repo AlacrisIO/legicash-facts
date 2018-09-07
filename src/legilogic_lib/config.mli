4492c7ae111d768473153865ae96bda54ae4820e
Paul Steckler <steck@stecksoft.com>
2018-09-07 11:38:43 -0400

    typo fix

--- src/legilogic_lib/config.mli
+++ src/legilogic_lib/config.mli
@@ -3,5 +3,5 @@
 val get_config_filename : string -> string
 (** get full path of config file, relies on $LEGICASH_CONFIG_DIR,
     which is provided by the Makefile at make time; at run-time,
-    it should be provided by the shall
+    it should be provided by the shell
 *)
