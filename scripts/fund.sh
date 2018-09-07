4161b0e801cd34504106f2bfb89d635066bd591b
Francois-Rene Rideau <fare@tunes.org>
2018-09-07 21:09:37 +0000

    Reduce build verbosity
    Makefile, scripts: kill stealthily
    
    gitignore _ethereum
    
    Make fund.sh executable, use new hex json format

--- scripts/fund.sh
+++ scripts/fund.sh
@@ -3,10 +3,10 @@
 
 curl -s -X POST -H 'Content-Type: application/json' -i http://localhost:8081/api/deposit --data '{
     "address": "0xc54e86dffb87b9736e2e35dd85c775358f1c31ce",
-    "amount": 999999
+    "amount": "0xFFFFF"
 }'
 
 curl -s -X POST -H 'Content-Type: application/json' -i http://localhost:8081/api/deposit --data '{
     "address": "0x9ccaed210ce8c0cb49c5ad1c4f583406c264ba69",
-    "amount": 999999
+    "amount": "0xFFFFF"
 }'
