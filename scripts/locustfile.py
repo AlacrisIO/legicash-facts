695fb56dc7b111dce70e360a8da5d015cb1d973f
Francois-Rene Rideau <fare@tunes.org>
2018-09-08 23:40:05 +0000

    Add a sleep endpoint
    Add support for it in the locustscript, but commented out.
    Tweak the locustscript some more.

--- scripts/locustfile.py
+++ scripts/locustfile.py
@@ -1,27 +1,44 @@
-# Trivial script to use locust
+# Trivial script to use locust -*- Python -*-
 
 from locust import HttpLocust, TaskSet
 
-def alicepaysbob(self):
-                self.client.post('/api/payment',None,{"sender": "0x9ccaed210ce8c0cb49c5ad1c4f583406c264ba69","recipient": "0xc54e86dffb87b9736e2e35dd85c775358f1c31ce","amount": "0x1"})
+def alice_pays_bob(self):
+  self.client.post('/api/payment',None,{"sender": "0x9ccaed210ce8c0cb49c5ad1c4f583406c264ba69","recipient": "0xc54e86dffb87b9736e2e35dd85c775358f1c31ce","amount": "0x1"})
 
 
-def bobpaysalice(self):
-                self.client.post('/api/payment',None,{"sender": "0xc54e86dffb87b9736e2e35dd85c775358f1c31ce","recipient": "0x9ccaed210ce8c0cb49c5ad1c4f583406c264ba69","amount": "0x1"})
+def bob_pays_alice(self):
+  self.client.post('/api/payment',None,{"sender": "0xc54e86dffb87b9736e2e35dd85c775358f1c31ce","recipient": "0x9ccaed210ce8c0cb49c5ad1c4f583406c264ba69","amount": "0x1"})
+
+
+def sleep_request(self):
+  self.client.post('/api/sleep',None,{})
 
 
 class AlicePaysBobTask(TaskSet):
-        tasks = {alicepaysbob : 1}
+  tasks = {alice_pays_bob : 5000}
+
 
 class BobPaysAliceTask(TaskSet):
-        tasks = {bobpaysalice : 1}
+  tasks = {bob_pays_alice : 5000}
+
+
+class SleepRequest(TaskSet):
+  tasks = {sleep_request : 5000}
+
 
 class AliceUser(HttpLocust):
-    task_set = AlicePaysBobTask
-    min_wait = 50
-    max_wait = 500
+  task_set = AlicePaysBobTask
+  min_wait = 50
+  max_wait = 500
+
 
 class BobUser(HttpLocust):
-    task_set = BobPaysAliceTask
-    min_wait = 50
-    max_wait = 500
+  task_set = BobPaysAliceTask
+  min_wait = 50
+  max_wait = 500
+
+
+#class Sleep(HttpLocust):
+#  task_set = SleepRequest
+#  min_wait = 50
+#  max_wait = 1000
