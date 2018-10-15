9ff4d402875ee1fc20dd1f23c044521975249074
Alex Coventry <alx@mit.edu>
2018-10-15 12:47:36 -0400

    Update deposit-test.py to new API response.

--- scripts/deposit-test.py
+++ scripts/deposit-test.py
@@ -1,12 +1,14 @@
 import requests, json, time
-deposit_data = {  "address": "0xc54e86dffb87b9736e2e35dd85c775358f1c31ce",
-                   "amount": "0x1000" }
-r = requests.post('http://localhost:8081/api/deposit', json=deposit_data)
+deposit_data = {  "address": "0x9ccaed210ce8c0cb49c5ad1c4f583406c264ba69",
+                   "amount": "0x1000000000" }
+
+url = 'http://ec2-52-203-232-2.compute-1.amazonaws.com:8081/api/'
+r = requests.post(url + 'deposit', json=deposit_data)
 
 delay = 0.001
 while True:
     time.sleep(0.1)
-    s = requests.get('http://localhost:8081/api/thread?id=%s' % r.json()['result']['thread'])
+    s = requests.get(url + 'thread?id=%s' % r.json()['result']['thread'])
     delay *= 2
     if s.json() != {'result': 'The operation is pending'}:
         break
