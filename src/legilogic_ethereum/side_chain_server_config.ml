376bece5b59362e97ae074f6a14edfaa0731fdcd
Mathieu Dutour Sikiric <Mathieu.Dutour@gmail.com>
2018-12-08 10:36:09 +0100

    Add the missed file.
      Mathieu

---
+++ src/legilogic_ethereum/side_chain_server_config.ml
@@ -0,0 +1,44 @@
+(* side_chain_server_config -- TCP/IP server to receive client requests *)
+
+open Legilogic_lib
+open Types
+
+module Side_chain_server_config = struct 
+
+  type ethereum_parameter_config =
+    { minimal_number_block_for_confirmation : int }
+  [@@deriving of_yojson]
+
+  type leveldb_parameter_config =
+    { batch_timeout_trigger_in_seconds : float
+    ; batch_size_trigger_in_requests : int
+    }
+  [@@deriving of_yojson]
+
+  type side_chain_server_config =
+    { port : int
+    ; eth_param_conf : ethereum_parameter_config
+    ; leveldb_param_conf : leveldb_parameter_config
+    }
+  [@@deriving of_yojson]
+
+  let config =
+    "side_chain_server_config.json"
+    |> Config.get_config_filename
+    |> Yojsoning.yojson_of_file
+    |> side_chain_server_config_of_yojson
+    |> function
+    | Ok config -> config
+    | Error msg -> Lib.bork "Error loading side chain server configuration: %s" msg
+
+  let sockaddr = Unix.(ADDR_INET (inet_addr_any, config.port))
+
+  (* let minNbBlockConfirm = lazy (match config with lazy {minimal_number_block_for_confirmation={nb}} -> nb) *)
+  let (minNbBlockConfirm : Revision.t) = Revision.of_int config.eth_param_conf.minimal_number_block_for_confirmation
+
+  let (batch_timeout_trigger_in_seconds : float) = config.leveldb_param_conf.batch_timeout_trigger_in_seconds
+
+  let (batch_size_trigger_in_requests : int) = config.leveldb_param_conf.batch_size_trigger_in_requests
+                        
+end
+                          
