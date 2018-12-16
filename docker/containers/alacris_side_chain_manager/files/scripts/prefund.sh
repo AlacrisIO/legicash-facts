06cf240e2d273ea4a88c1b31e46935b1e674e8a9
Mathieu Dutour Sikiric <Mathieu.Dutour@gmail.com>
2018-12-16 17:13:06 +0100

    Now the system is back to running.
      Mathieu

--- docker/containers/alacris_side_chain_manager/files/scripts/prefund.sh
+++ docker/containers/alacris_side_chain_manager/files/scripts/prefund.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-cd /var/www/app/legicash-facts && ./_run/ethereum_prefunder.exe ./config/facilitator_keys.json ./config/demo-keys-small.json
+cd /var/www/app/legicash-facts && ./_run/ethereum_prefunder.exe ./config/operator_keys.json ./config/demo-keys-small.json
 
 exec "$@"
