ab5a2e94d9da9c2b8513cf6e53411f046f3789cd
Lucy Zhang <lucy5813@gmail.com>
2019-03-09 10:47:55 -0500

    Create tests to check for TransactionRejected
    Created tests `contract-success-on-Ethereum-testnet` and
    `contract-failure-on-Ethereum-testnet` in `ethereum_user.ml`. The
    failure test uses a bogus gas_limit to trigger a TransactionReceipt
    with a status of 0, throwing `TransactionRejected`, which then returns
    finally with `TransactionFailed`.
    
    I left the test `create-contract-on-Ethereum-testnet` even though it
    is redundant now. I planned on removing it when I was done.

--- contracts/hello.sol
+++ contracts/hello.sol
@@ -6,4 +6,25 @@ contract HelloWorld {
     function printHello() public pure returns (string memory) {
         return "Goodbye!";
     }
+
+    function printHelloNum(int num) public pure returns (int) {
+        return num;
+    }
+
+    function printHelloName(string memory _name) public returns (string memory) {
+        string memory s = strConcat("Hello, ", _name);
+        emit showResult(s);
+        return "Goodbye!";
+    }
+
+    function strConcat(string memory _a, string memory _b) internal pure returns (string memory) {
+        bytes memory _ba = bytes(_a);
+        bytes memory _bb = bytes(_b);
+        string memory ab = new string(_ba.length + _bb.length);
+        bytes memory bab = bytes(ab);
+        uint k = 0;
+        for (uint i = 0; i < _ba.length; i++) bab[k++] = _ba[i];
+        for (uint i = 0; i < _bb.length; i++) bab[k++] = _bb[i];
+        return string(bab);
+    }
 }
