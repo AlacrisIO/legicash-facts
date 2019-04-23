43642ed4cfbc8965c6a2913e792bef321ac5d091
Matt Audesse <matt@mattaudesse.com>
2019-04-23 09:15:51 -0400

    Add TODO RE: duplicate claims regression in contracts/claims.sol
    See:
    https://gitlab.com/legicash/legicash-facts/merge_requests/214/diffs#note_161909798

--- contracts/claims.sol
+++ contracts/claims.sol
@@ -98,7 +98,10 @@ contract Claims {
      * Usage Pattern: make_claim(digest_claim(operator, tag, keccak256(abi.encodePacked(x, y, z)))).
      */
     function make_claim(bytes32 _claim) internal {
-//        require(claim_status[_claim]==0); // The claim must not have been made before
+        // TODO ensure duplicate claims cannot be submitted and provide
+        // automated tests to detect future regressions
+        // See: https://gitlab.com/legicash/legicash-facts/merge_requests/214/diffs#note_161909798
+        // require(claim_status[_claim]==0); // The claim must not have been made before
         claim_status[_claim] = int(now) + challenge_period_in_seconds; // Register the claim
     }
 
