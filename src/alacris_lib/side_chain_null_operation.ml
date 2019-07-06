3641bdf41e3fc72a2d6632a76505cee78933c035
Mathieu Dutour Sikiric <Mathieu.Dutour@gmail.com>
2019-07-06 13:53:08 +0200

    Insertion of the code for null_operations.
      Mathieu

---
+++ src/alacris_lib/side_chain_null_operation.ml
@@ -0,0 +1,45 @@
+open Legilogic_lib
+open Action
+open Signing
+
+open Legilogic_ethereum
+open Side_chain_server_config
+open Ethereum_watch
+open Side_chain
+
+
+
+let small_money_transfer_first_second : recipient:Address.t -> sender:Address.t -> unit Lwt_exn.t =
+  fun ~recipient ~sender ->
+  let open Lwt_exn in
+  let transfer_amount = TokenAmount.of_string "500000000000" in
+  let pre_transaction = Ethereum_user.transfer_tokens ~recipient transfer_amount in
+  Ethereum_user.post_pretransaction pre_transaction sender
+  >>= fun _ -> return ()
+
+
+let small_money_transfer : int -> unit Lwt_exn.t =
+  fun pos ->
+  let alice_address = Signing.Test.alice_address in
+  let bob_address = Signing.Test.bob_address in
+  if (pos == 0) then
+    small_money_transfer_first_second ~recipient:alice_address ~sender:bob_address
+  else
+    small_money_transfer_first_second ~recipient:bob_address ~sender:alice_address
+
+
+let rec infinite_loop_transfer : int -> unit Lwt_exn.t =
+  fun pos ->
+  let open Lwt_exn in
+  small_money_transfer pos
+  >>= fun () -> sleep_delay_exn 5.0
+  >>= fun () -> infinite_loop_transfer (1 - pos)
+
+
+
+let start_null_operation_thread () =
+  Logging.log "Beginning of the inner_vigilant_thread";
+  if Side_chain_server_config.run_null_operation then
+    Lwt.async (fun () -> infinite_loop_transfer 0);
+  Lwt_exn.return ()
+
